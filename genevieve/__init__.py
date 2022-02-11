b1384d990310504e6c713e49b010fb7a338fe916
Krzysztof Czarnecki <krzysztof.czarnecki@technica-engineering.de>
2022-02-11 23:26:37 +0100

    test: increase coverage

--- genevieve/__init__.py
+++ genevieve/__init__.py
@@ -10,7 +10,7 @@ __version__ = "1.0.0"
 
 import argparse
 from os import PathLike
-from typing import Any, Dict, Union
+from typing import Any, Callable, Dict, Union
 import yaml
 
 
@@ -19,6 +19,7 @@ def assert_key_exists(parsed_yaml: Dict[str, Any], key: str):
         raise KeyError(key)
 
 
+# TODO: allow platform-scoped variables!! e.g. `windows`, `linux`, `macos` keys at the same level as `variables`
 def generate_sh(parsed_yaml: Dict[str, Any], query_key: str) -> str:
     variables = parsed_yaml[query_key]
     str_sh = ""
@@ -41,38 +42,40 @@ def parse_yaml(path: Union[str, PathLike]) -> Dict[str, Any]:
     return parsed_yaml
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Generates env.env and env.ps1")
-    parser.add_argument("-f", "--file",
-                        help="Input YAML file", required=True)
-    parser.add_argument("-o", "--output",
-                        help="Output filename (without extension)")
-    parser.add_argument("-d", "--debug",
-                        help="Debug mode, prints to stdout instead", action="store_true")
+def main(printer: Callable):
+    def _main():
+        parser = argparse.ArgumentParser(description="Generates env.env and env.ps1")
+        parser.add_argument("-f", "--file",
+                            help="Input YAML file", required=True)
+        parser.add_argument("-o", "--output",
+                            help="Output filename (without extension)")
+        parser.add_argument("-d", "--debug",
+                            help="Debug mode, prints to stdout instead", action="store_true")
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    parsed_yaml = parse_yaml(args.file)
+        parsed_yaml = parse_yaml(args.file)
 
-    variables_query_key = "variables"
-    assert_key_exists(parsed_yaml, variables_query_key)
+        variables_query_key = "variables"
+        assert_key_exists(parsed_yaml, variables_query_key)
 
-    str_sh = generate_sh(parsed_yaml, variables_query_key)
-    str_pwsh = generate_pwsh(parsed_yaml, variables_query_key)
+        str_sh = generate_sh(parsed_yaml, variables_query_key)
+        str_pwsh = generate_pwsh(parsed_yaml, variables_query_key)
 
-    filename = args.output or "env"
+        filename = args.output or "env"
 
-    if args.debug:
-        print(str_sh)
-        print(str_pwsh)
-        exit(0)
+        if args.debug:
+            printer(str_sh)
+            printer(str_pwsh)
+            exit(0)
 
-    with open(f"{filename}.sh", "w") as f:
-        f.write(str_sh)
+        with open(f"{filename}.sh", "w") as f:
+            f.write(str_sh)
 
-    with open(f"{filename}.ps1", "w") as f:
-        f.write(str_pwsh)
+        with open(f"{filename}.ps1", "w") as f:
+            f.write(str_pwsh)
+    return _main
 
 
 if __name__ == "__main__":
-    main()
+    main(print)
